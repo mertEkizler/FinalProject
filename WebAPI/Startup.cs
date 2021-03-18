d434e816e7e2f2623871553261ba85a647b94803
mert ekizler <mertekizler@hotmail.com>
2021-03-18 15:30:25 +0300

    categories controller eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -74,7 +74,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(builder=>builder.WithOrigins("http://localhost:58921").AllowAnyHeader());
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
